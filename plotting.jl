c4bf892606f8f4f079b8ba69fd4c0eb190bc1397
ymeysa25 <40119560+ymeysa25@users.noreply.github.com>
2020-10-28 14:31:33 +0700

    update

--- plotting.jl
+++ plotting.jl
@@ -1,1 +1,8 @@
-using Plots
+using Gadfly
+using RDatasets
+
+iris = dataset("datasets", "iris")
+p = plot(iris, x=:SepalLength, y=:SepalWidth, Geom.point)
+# img = SVG("iris_plot.svg", 6inch, 4inch)
+jpg = PNG("iris_plot.jpg", 6inch, 4inch)
+draw(img, jpg)
